2614bd606e13d693538e1d08362edf0810a147f0
Ian S Pringle <pard@0x44.pw>
2021-10-21 10:27:33 -0500

    Removed defer_fn load and loading colorscheme first and last now

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,6 +2,7 @@
 -- later on speeds up start-up times
 -- Never needed
 vim.g.loaded_gzip = false
+vim.g.loaded_netrw = false
 vim.g.loaded_netrwPlugin = false
 vim.g.loaded_tarPlugin = false
 vim.g.loaded_zipPlugin = false
@@ -9,16 +10,16 @@ vim.g.loaded_2html_plugin = false
 vim.g.loaded_remote_plugins = false
 
 -- Deferred loading
-vim.opt.syntax = "off"
+-- vim.opt.syntax = "off"
 -- vim.opt.filetype = "off"
 -- vim.opt.filetype = "plugin indent off"
 -- vim.opt.shadafile = "NONE"
 
-vim.defer_fn(function()
-  require('settings')
-  require('plugins')
-  require('mappings')
-  require('modules')
-end, 0)
+-- So much random stuff depends on the colorscheme being loaded before _or_
+-- after, but not either. So I'll just load it twice.
+require('modules/aesthetic/plugins/colorscheme').setup()
 
-
+require('settings')
+require('plugins')
+require('mappings')
+require('modules')

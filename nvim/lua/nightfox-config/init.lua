c76c3cd5de1b6e8c38d0985997b228aa660e0586
neobats <batton.guy@gmail.com>
2022-07-14 09:39:02 -0400

    Updated nightfox-config

--- nvim/lua/nightfox-config/init.lua
+++ nvim/lua/nightfox-config/init.lua
@@ -1,11 +1,13 @@
 require('nightfox').setup({
   options = {
     terminal_colors = true, -- Set terminal colors (vim.g.terminal_color_*) used in `:terminal`
-    dim_inactive = false, -- Non focused panes set to alternative background
+    dim_inactive = true, -- Non focused panes set to alternative background
     styles = {
       comments = "italic",
+      functions = "italic",
       types = "italic,bold",
       constants = "bold",
+      operators = "bold",
     }
   }
 })

10f9d6ec7d08f91d7fd75d7f80d4cf849a538885
Ian S. Pringle <ipringle@jackhenry.com>
2022-12-08 15:30:17 -0600

    Removing old configs, probably won't need them for reference anymore

--- nvim/lua-old/modules/file/plugins/init.lua
+++
@@ -1,9 +0,0 @@
-local M = {}
-
-local tree = require('modules/file/plugins/tree')
-
-function M.setup()
-  tree.setup()
-end
-
-return M
--- nvim/lua-old/modules/terminal/init.lua
+++
@@ -1,9 +0,0 @@
-local M = {}
-
-local plugins = require('modules/terminal/plugins')
-
-function M.setup()
-  plugins.setup()
-end
-
-return M
